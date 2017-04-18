0363216fa48e7f94d54e2f042c3ba6cf394a4752
loren pabst <loren pabst>
2017-04-18 16:02:49 -0600

    first half is done and working

--- basic-js-review/review.js
+++ basic-js-review/review.js
@@ -1,30 +1,40 @@
 /////////////Variables/////////////
 //1. Write a variable called color with a value of "dark blue".
-
+var color = 'dark blue';
 //2. Write a variable, myFave, that has the value "My favorite color is "
-
+var myFave = 'My favorite color is';
 //3. Use the variables from 1 and 2 to make myFavoriteColor equal
 //   "My favorite color is dark blue" (concatenate)
-var myFavoriteColor;
+var myFavoriteColor = myFave + " " + color;
 
 //4. Create an array of strings that includes the letters a - d in order.
-var aThroughD;
+var aThroughD = ['a', 'b', 'c', 'd'];
 //5. Remove 'a' and store it to a variable called isForApple.
-
+var isForApple = aThroughD.shift();
 //6. Add 'e' to the end of aThroughD.
-
+aThroughD.push('e');
 //7. Add 'apple' to the beginning of aThroughD
-
+aThroughD.unshift('apple');
 //8. Remove 'c' from the array and put 'cat' in its place
-
+for (var i in aThroughD){
+  if (aThroughD[i] === 'c'){
+    aThroughD[i] = 'cat';
+  }
+}
 //9. Leave 'apple' in the array but put a copy of it on a var called bestFruit
-
+var bestFruit = aThroughD[aThroughD.indexOf('apple')];
 //10. Create an object called spaceJam
 //    spaceJam has: a title of 'Space Jam' (String)
 //                  a release_year of 1996 (Number)
 //                  is not an oscarAwardWinner (Boolean)
 //                  and has the stars Michael Jordan, Bugs Bunny, and Bill Murray (array of strings)
 
+var spaceJam = {
+  title: 'Space Jam',
+  release_year: 1996,
+  oscarAwardWinner: false,
+  stars: ['Michael Jordan', 'Bugs Bunny', 'Bill Murray']
+}
 
 //11. Change anakin's side to dark using bracket notation
 
@@ -36,16 +46,30 @@ var anakinSkywalker =  {
   former_mother: 'Shmi Skywalker'
 }
 
+anakinSkywalker['side'] = 'dark';
+
 //12. Add 'Jar Jar' to his children using dot notation
-
+anakinSkywalker.children.push('Jar Jar');
+console.log(anakinSkywalker.children);
 //13. Loop through the object and remove any property that starts with former
-
+for (var key in anakinSkywalker){
+  if (key.startsWith("former")){
+    delete anakinSkywalker[key];
+  }
+}
 
 
 //14. Create a function called finder that takes in an array and a string and loops through the array
 //    to remove every instance of that string
 
-
+function finder(arr, str){
+  for (var i = arr.length - 1; i >= 0; i --){
+    if (arr[i] === str){
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+}
 
 //15. Write a function called numChecker that takes in a parameter and checks to see if it is a number or a
   //number in a string (ie '12' or 12) - if so, return true, else return false
